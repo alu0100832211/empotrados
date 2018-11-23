84f8e53b803caca7db647ab87850e45378896a74
Martin Belda <alu0100832211@ull.edu.es>
2018-11-23 16:58:11 +0000

    Metodos obtener tiempo y esperar

--- Ejemplos/temporizador.c
+++ Ejemplos/temporizador.c
@@ -11,10 +11,13 @@
 #include <sys/sio.h>
 #include <sys/locks.h>
 
-
+//Para pensar mejor
 typedef unsigned char byte;
+typedef unsigned int 2bytes;
+typedef unsigned long 4bytes;
 
-unsigned long nDesbordamientos = 0UL;
+//Nº máximo desbordamientos 2⁸
+4bytes nDesbordamientos = 0UL;
 
 void __attribute__((interrupt)) vi_tov(void) {
 	nDesbordamientos++;
@@ -39,12 +42,74 @@ void init_temporizador(void){ //futuro: en vez de void int factor
 }
 
 //Devuelve los microsegundos quehan pasado
-unsigned long get_microsenconds(void){
-	//Lo que hay en TCNT son el nº de ciclos que ha pasado
-	//Depende del factor el tiempo de ciclo es diferente
-	//Multiplicar nº de ciclos totales por tiempo de ciclo
-	//nº de ciclos totales será 2¹⁶ * nDesbordamientos
+4bytes get_microseconds(void){
+    //Obtenemos el factor de frecuencia
 	byte factorT = _io_ports[M6812_TMSK2] & 0x07;
-	unsigned long frec = M6812_CPU_E_CLOCK/(1<<factorT);		
-	unsigned long  numCiclosL = (nDesbordamientos << 16) | _IO_PORTS_W();
+    //Frecuencia es frecuencia total(8MHz)/2^factor
+	4bytes frec = M6812_CPU_E_CLOCK/(1<<factorT);		
+    //Numero de ciclos total es el valor actual del contador
+    //concatenado por la izquierda con el nº desbordamientos
+	4bytes  numCiclos = (nDesbordamientos << 16) | _IO_PORTS_W(M6812_TCTL);
+    //Microsegundos depende del factor
+    int desp = 3 - factorT;
+    4bytes microSeconds;
+    if (desp > 0){
+       return numCiclos >> desp;
+    }
+    else{
+        desp *= -1;
+        return numCiclos << desp;
+    }
 }
+
+//Devuelve los milisegundos que han pasado
+4bytes get_miliseconds(void){
+    //Parar el temporizador hasta volver de esta funcion?
+    return get_microseconds() * 1000;
+}
+
+//Metodo para realizar esperas solicitadas -> el del ejemplo
+
+void delayusg(unsigned long useg) {
+  unsigned int numCiclos;
+  unsigned long numCiclosL;
+
+  /* Desconectamos para no afectar al pin */
+  _io_ports[M6812_TCTL1] &= ~(M6812B_OM6 | M6812B_OL6);
+
+  /* Vemos velocidad del temporizador*/
+  byte factorT = _io_ports[M6812_TMSK2] & 0x07; /*Factor de escalado actual*/
+  unsigned long frec = M6812_CPU_E_CLOCK/(1 << factorT); /* Frecuencia del temporizador*/
+  /* Según la frecuencia elegimos el modo de dividir para evitar desbordamientos */
+  if(frec/1000000)
+    numCiclosL = frec/1000000 * useg;
+  else
+    numCiclosL = frec/100 * useg/10000;
+
+  unsigned int numDisparos = numCiclosL >> 16;  /* Numero de disparos necesarios */
+  numCiclos = numCiclosL & 0xffff; /* Número restante de ciclos */
+
+  /* Por si escalado muy grande y useg pequeño */
+  if((numCiclos == 0) && (numDisparos == 0)) numCiclos = 1;
+
+  _io_ports[M6812_TIOS] |= M6812B_IOS6; /*configuramos canal como comparador salida*/
+  _io_ports[M6812_TFLG1] = M6812B_C6F; /*Bajamos el banderín  */
+  /*preparamos disparo*/
+  _IO_PORTS_W(M6812_TC6) = _IO_PORTS_W(M6812_TCNT) + numCiclos ;
+
+  /*Habilitamos el temporizador, por si no lo está*/
+  _io_ports[M6812_TSCR] |= M6812B_TEN;
+
+  /* Esparamos los desboradmientos necesarios */
+  do {
+    /* Nos quedamos esperando a que se produzca la igualdad*/
+    while (! (_io_ports[M6812_TFLG1] & M6812B_C6F));
+    _io_ports[M6812_TFLG1] = M6812B_C6F; /* Bajamos el banderín */
+  } while(numDisparos--);
+}
+
+//Instalar funcion que se ejecute tras tiempo especificado
+
+
+//Instalar funcion que se realice periódicamente
+
--- Ejemplos/temporizador.h
+++ Ejemplos/temporizador.h
@@ -1,1 +1,11 @@
-extern unsigned long nDesbordamientos; 
+//extern unsigned long nDesbordamientos; 
+//no hace falta acceder a esta variable 
+//fuera de la libreria, solo se usa para
+//obtener el tiempo en useg, mseg, etc.
+
+
+void __attribute__((interrupt)) vi_tov(void);
+void init_temporizador(void);
+4bytes get_microseconds(void);
+4bytes get_miliseconds(void);
+void delayusg(unsigned long useg) ;
