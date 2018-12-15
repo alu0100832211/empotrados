a3f1537e0319156b3f64faa6951caea459e7673c
Martin <alu0100832211@ull.edu.es>
2018-12-15 00:26:13 +0000

    fix

--- pruebaLibs/funcionAyB.c
+++ pruebaLibs/funcionAyB.c
@@ -2,41 +2,44 @@
 #include <temporizador.h>
 
 void funcionEjemplo(void * args){
-	serial_print("a\n");
+  serial_print("a\n");
 }
 
 void funcionEjemplo2(void * args){
   unsigned char * byte;
-  args = (unsigned char*) args
+  args = (unsigned char*) args;
 
-	serial_print(byte);
+  serial_print(*byte);
 }
 
 int main () {
-	lock ();
+  lock ();
 
-	_io_ports[M6812_DDRG] |= M6812B_PG7;
-	_io_ports[M6812_PORTG] |= M6812B_PG7;
+  _io_ports[M6812_DDRG] |= M6812B_PG7;
+  _io_ports[M6812_PORTG] |= M6812B_PG7;
 
-	serial_init();
-	serial_print("\nDelayUsg.c ============\n");
+  serial_init();
+  serial_print("\nDelayUsg.c ============\n");
 
-	init_temporizador(7);
+  init_temporizador(7);
 
-	unlock();
+  unlock();
 
-	serial_print("\n\rTerminada inicialización\n");
+  serial_print("\n\rTerminada inicialización\n");
 
   unsigned char * byte = (unsigned char*)malloc(sizeof(unsigned char));
-	runAfterUsg(funcionEjemplo, NULL, 1000UL*2000UL);
-	runEveryUsg(funcionEjemplo2, , 1000UL*3000UL);
+  *byte = b;
+  runAfterUsg(funcionEjemplo, NULL, 1000UL*2000UL);
+  runEveryUsg(funcionEjemplo2, byte, 1000UL*3000UL);
 
-	delayusg(1000UL*1000UL);
-	print4bWord(get_microseconds());
+  delayusg(1000UL*1000UL);
+  print4bWord(get_microseconds());
 
-	while(1){
-		_io_ports[M6812_PORTG] ^= M6812B_PG7;
-		print4bWord(get_microseconds());
-		delayusg(5000UL * 1000UL);
-	}
+  while(1){
+    _io_ports[M6812_PORTG] ^= M6812B_PG7;
+    print4bWord(get_microseconds());
+    delayusg(5000UL * 1000UL);
+  }
+
+  free(byte);
 }
