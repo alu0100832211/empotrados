5df4ee370fc2b2e4905f788c0b5f74eaf2bc35e3
Martin Belda <alu0100832211@ull.edu.es>
2018-12-21 09:20:57 +0000

    acotar

--- pruebaLibs/probarPWM.c
+++ pruebaLibs/probarPWM.c
@@ -6,6 +6,8 @@ int main(void){
     serial_print("Introducir valor (0-5000):\n");
 
     unsigned long frec = serial_recv();
+    if (frec > 100) frec = 100;
+    else if (frec < 0) frec = 0;
 //void pwd_generalizado(unsigned int channel, unsigned long frec, unsigned int pol);
     //pin 40 PP0/PW0
     pwd_generalizado(0, frec, 1);
